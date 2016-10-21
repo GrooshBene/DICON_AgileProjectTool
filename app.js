c2500923f58386ea935fd5d269ed3131b09d70f9
박지수 <wltn000129@gmail.com>
2016-10-21 23:24:52 +0900

    Server Basic Mongoose Setting Added in app.js

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var mongoose = require('mongoose');
+var randomString = require('randomstring');
+var schema = mongoose.Schema;
 
 var app = express();
+
+mongoose.connect("mongodb://localhost:27017/agile", function (err) {
+    if(err){
+        console.log("MongoDB Error");
+        throw err;
+    }
+});
+
+//requiring vars
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
